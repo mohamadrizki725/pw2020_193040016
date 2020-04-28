aa14fccab54d2f5946f5f50fece32a38e0965270
Moh_RizkiRamdani <mohamadrizki725@users.noreply.github.com>
2020-04-28 14:45:14 +0700

    menambahkan fungsi hapus, ubah dan pencarian

---
+++ kuliah/pertemuan11/index.php
@@ -0,0 +1,64 @@
+<?php
+require 'functions.php';
+$mahasiswa = query("SELECT * FROM mahasiswa");
+
+// ketika tombol cari di klik
+if (isset($_POST['cari'])) {
+  $mahasiswa = cari($_POST['keyword']);
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Daftar Mahasiswa</title>
+</head>
+
+<body>
+  <h3>Daftar Mahasiswa</h3>
+  <a href="tambah.php" style="text-decoration: none">Tambah data mahasiswa</a>
+  <br><br>
+
+  <form action="" method="POST">
+    <input type="text" name="keyword" size="40" placeholder="Masukan keyword pencarian..." autocomplete="off" autofocus>
+    <button type="submit" name="cari">Cari!!</button>
+
+  </form><br>
+
+  <table border="1" cellpadding="10" cellspacing="0">
+    <tr>
+      <th>#</th>
+      <th>Gambar</th>
+      <th>Nama</th>
+      <th>Aksi</th>
+    </tr>
+
+    <?php if (empty($mahasiswa)) : ?>
+      <tr>
+        <th colspan="4">
+          <h3>Data mahasiswa tidak ditemukan!</h3>
+        </th>
+      </tr>
+    <?php endif; ?>
+
+    <?php $i = 1;
+    foreach ($mahasiswa as $m) : ?>
+
+      <tr>
+        <td><?= $i++; ?></td>
+        <td><img src="img/<?= $m['gambar']; ?>" width="60px"></td>
+
+        <td>
+          <?= $m['nama']; ?>
+        </td>
+        <td><a href="detail.php?id=<?= $m['id']; ?>">lihat detail</a></td>
+
+      </tr>
+    <?php endforeach; ?>
+  </table>
+</body>
+
+</html>
